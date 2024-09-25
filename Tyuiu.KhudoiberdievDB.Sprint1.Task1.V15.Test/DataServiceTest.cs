e358a142cd359c7bb359422f7673b5c6c4950c5b
Tyuiu.KhudoyberdievDB@outlook.com <Tyuiu.KhudoyberdievDB@outlook.com>
2024-09-25 21:46:58 +0500

    Task1

---
+++ Tyuiu.KhudoiberdievDB.Sprint1.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.KhudoiberdievDB.Sprint1.Task1.V15.Lib;
+namespace Tyuiu.KhudoiberdievDB.Sprint1.Task1.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.5;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
